2bf53bcd2c963a882a7274c40aa2a97bba24676d
Ranashahira Reztaputri <ranashahira.rez@gmail.com>
2026-05-24 23:25:26 +0700

    feat: integrate

--- chiba-trash-management/loading_screen.gd
+++ chiba-trash-management/loading_screen.gd
@@ -22,3 +22,5 @@ func _process(delta):
 		
 		set_process(false) 
 		print("LOADING SELESAI")
+		#await get_tree().create_timer(0.5).timeout
+		get_tree().change_scene_to_file("res://scenes/base_level.tscn")
