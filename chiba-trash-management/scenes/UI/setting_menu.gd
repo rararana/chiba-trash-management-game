c92d525743cbe3bd06ee362b33afe7fa9f36e1eb
Indana <18223100@std.stei.itb.ac.id>
2026-06-10 21:37:49 +0700

    fix setting

--- chiba-trash-management/scenes/UI/setting_menu.gd
+++ chiba-trash-management/scenes/UI/setting_menu.gd
@@ -3,10 +3,18 @@ extends Control
 @onready var menu_audio = $SettingOverlay/MenuAudio
 @onready var menu_language = $SettingOverlay/MenuLanguage
 
+var master_bus_index: int
+
 func _ready():
 	hide()
 	menu_audio.show()
 	menu_language.hide()
+	
+	master_bus_index = AudioServer.get_bus_index("Master")
+	
+func _on_h_slider_value_changed(value: float) -> void:
+	var volume_db = linear_to_db(value)
+	AudioServer.set_bus_volume_db(master_bus_index, volume_db)
 
 func _on_tombol_garis_3_pressed():
 	menu_audio.hide()
